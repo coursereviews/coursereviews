d55147a815630a7ca20c96a011980d509722c131
danasilver <dsilver1221@gmail.com>
2013-11-02 20:15:13 -0400

    Use EdgeNgramFields to do partial search matches.

--- coursereviews/apps/reviews/search_indexes.py
+++ coursereviews/apps/reviews/search_indexes.py
@@ -3,7 +3,7 @@ from haystack import indexes
 from reviews.models import Course, Professor
 
 class CourseIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
+    text = indexes.EdgeNgramField(document=True, use_template=True)
     code = indexes.CharField(model_attr="code")
     title = indexes.CharField(model_attr="title")
     description = indexes.CharField(model_attr="description")
@@ -16,7 +16,7 @@ class CourseIndex(indexes.SearchIndex, indexes.Indexable):
         return self.get_model().objects.all()
 
 class ProfessorIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
+    text = indexes.EdgeNgramField(document=True, use_template=True)
     first_name = indexes.CharField(model_attr="first")
     last_name = indexes.CharField(model_attr="last")
     email = indexes.CharField(model_attr="email")
