178c27542075a201398f1dea0b58e18c99c757a7
danasilver <dsilver1221@gmail.com>
2014-03-02 23:40:45 -0500

    Add admin templates

--- coursereviews/apps/admin/urls.py
+++ coursereviews/apps/admin/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('admin.views',
+    url(r'^$', 'index', name='admin_index'),
     url(r'^quota$', 'quota', name='admin_quota'),
     url(r'^flags$', 'flags', name='admin_flags'),
 )
--- coursereviews/apps/admin/views.py
+++ coursereviews/apps/admin/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 from reviews.models import Review
+
+def index(request):
+    
+    return TemplateResponse(request, 'cr_admin/index.html')
 
 def quota(request):
     return HttpResponse(status=200)
