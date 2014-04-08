5f8f65335c5190fc7f949bc0e1f54971f400759c
danasilver <dsilver1221@gmail.com>
2014-04-07 20:34:58 -0400

    Searchable review admin interface.

--- coursereviews/apps/reviews/admin.py
+++ coursereviews/apps/reviews/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from reviews.models import *
 
-class ReviewsAdmin(admin.ModelAdmin):
-    pass
+class ReviewAdmin(admin.ModelAdmin):
+    search_fields = ('course__code', 'prof__first', 'prof__last')
 
-admin.site.register(Professor, ReviewsAdmin)
-admin.site.register(Review, ReviewsAdmin)
-admin.site.register(Course, ReviewsAdmin)
-admin.site.register(ProfCourse, ReviewsAdmin)
-admin.site.register(Department, ReviewsAdmin)
+class ProfCourseAdmin(admin.ModelAdmin):
+    search_fields = ('prof_course__prof__first', 'prof_course__prof__last',
+                     'prof_course__course__code', 'user__username')
+
+class ProfAdmin(admin.ModelAdmin):
+    search_fields = ('first', 'last')
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ('code', 'title')
+
+class DeptAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+admin.site.register(Professor, ProfAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(ProfCourse, ProfCourseAdmin)
+admin.site.register(Department, DeptAdmin)
