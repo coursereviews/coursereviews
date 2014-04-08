342df1ead4c5602297b62b0eccefcce56904c88f
danasilver <dsilver1221@gmail.com>
2014-04-07 22:09:32 -0400

    Redirect admin to login if not logged in and add spaces between subheadings.

--- coursereviews/apps/cr_admin/views.py
+++ coursereviews/apps/cr_admin/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.db.models import Count
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import (get_object_or_404,
                               redirect)
 
@@ -11,10 +12,12 @@ from cr_admin.models import AdminQuota
 from cr_admin.decorators import (middcourses_admin_required,
                                  middcourses_moderator_required)
 
+@login_required
 @middcourses_moderator_required
 def index(request):
     return TemplateResponse(request, 'cr_admin/index.html')
 
+@login_required
 @middcourses_admin_required
 def quota(request):
     users_quota_count = UserProfile.objects.values('quota').order_by().annotate(Count('quota'))
@@ -27,16 +30,19 @@ def quota(request):
                                                              'quota_form': quota_form,
                                                              'current_admin_quota': admin_quota})
 
+@login_required
 @middcourses_moderator_required
 def flags(request):
     flagged_reviews = Review.objects.filter(flagged=True, flagged_mod=None)
     return TemplateResponse(request, 'cr_admin/flags.html', {'reviews': flagged_reviews})
 
+@login_required
 @middcourses_admin_required
 def flags_moderated(request):
     flagged_reviews = Review.objects.all().exclude(flagged_mod=None)
     return TemplateResponse(request, 'cr_admin/flags_moderated.html', {'reviews': flagged_reviews})
 
+@login_required
 @middcourses_moderator_required
 def flagged_review(request, review_id):
     review = get_object_or_404(Review, id=review_id)
