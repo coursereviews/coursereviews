2b947424d919385376e73b8aff9c3280ad32d9a7
danasilver <dsilver1221@gmail.com>
2014-04-07 20:38:37 -0400

    Fix admin mixup between prof course and review search fields

--- coursereviews/apps/reviews/admin.py
+++ coursereviews/apps/reviews/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from reviews.models import *
 
-class ReviewAdmin(admin.ModelAdmin):
+class ProfCourseAdmin(admin.ModelAdmin):
     search_fields = ('course__code', 'prof__first', 'prof__last')
 
-class ProfCourseAdmin(admin.ModelAdmin):
+class ReviewAdmin(admin.ModelAdmin):
     search_fields = ('prof_course__prof__first', 'prof_course__prof__last',
                      'prof_course__course__code', 'user__username')
 
