176b8ac392a6ed627818c8402380404b5a9f3172
Dana Silver <dsilver1221@gmail.com>
2014-08-22 18:51:53 -0400

    Initial vertical chart for #48. TODO: labels and interaction.

--- coursereviews/static/reviews/js/detail.js
+++ coursereviews/static/reviews/js/detail.js
@@ -17,7 +17,7 @@ $(function() {
   // D3.js, display the stats
   var $statsContainer = $("#stats-container"),
       barHeight = 30,
-      margin = {top: 10, right: 10, bottom: 20, left: 10};
+      margin = {top: 10, right: 10, bottom: 35, left: 10};
 
   var x = d3.scale.linear();
 
@@ -25,6 +25,7 @@ $(function() {
       .scale(x)
       .orient("bottom");
 
+  var allStats = [];
   var sortedStats = [];
   d3.json(window.location.protocol + '//' + window.location.hostname
         + (window.location.port ? ':' + window.location.port: '')
@@ -33,21 +34,113 @@ $(function() {
       .get(function(error, stats) {
         if (error) throw error;
 
-        stats = JSON.parse(stats);
+        allStats = stats = JSON.parse(stats);
         for (var key in stats) {
           if (key !== 'comments' &&
               key !== 'date' &&
-              key !== 'prof_course_id') {
+              key !== 'prof_course_id' &&
+              key !== 'hours') {
             sortedStats.push({'key': key, 'stats': stats[key]});
           }
         }
         sortedStats.sort(function(a,b) {
-          return questionLookup.keys().indexOf(a.key) - 
+          return questionLookup.keys().indexOf(a.key) -
                  questionLookup.keys().indexOf(b.key);
         });
 
+        if (stats.hasOwnProperty('hours'))
+          hoursChart(stats['hours']);
+
         makeCharts(sortedStats);
       });
+
+  function hoursChart(hoursStats) {
+    var margin = {top: 30, right: 10, bottom: 35, left: 30};
+    var height = 300 - margin.top - margin.bottom;
+    var width = $statsContainer.width() - 30;
+
+    var data = d3.entries(d3.range(13)).map(function (h) {
+      if (hoursStats.hasOwnProperty(h.key))
+        return {key: +h.key, value: hoursStats[h.key]};
+
+      return {key: +h.key, value: 0};
+    });
+
+    var x = d3.scale.linear()
+        .domain(d3.extent(data, function (d) { return d.key; }))
+        .range([width / data.length / 2, width - width / data.length / 2]);
+
+    var y = d3.scale.linear()
+        .domain([0, d3.max(data, function (d) { return d.value; })])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient('bottom');
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left')
+        .ticks(d3.max(data, function (d) { return d.value; }));
+
+    $statsContainer.append('<div><h4>' + questionLookup.get('hours') +
+                           '</h4><svg id="hours"></svg></div>');
+
+    var chart = d3.select('#hours')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .attr('class', 'middcourses-chart')
+      .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    var xAxisEl = chart.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+
+    xAxisEl.append('text')
+        .attr('dx', -5)
+        .attr('dy', 30)
+        .text('hours per week');
+
+    chart.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis);
+
+    chart.append('g')
+        .attr('class', 'y gridlines')
+        .call(yAxis.tickFormat('').tickSize(-width, 0, 0))
+        .style('stoke', 'grey');
+
+    chart.selectAll('rect')
+        .data(data)
+      .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', function (d) { return Math.floor((x(d.key) - width / data.length / 2)) + 1; })
+        .attr('y', function (d) { return y(d.value)})
+        .attr('width', Math.round((width / data.length)) - 2)
+        .attr('height', function (d) { return height - y(d.value)});
+
+    var avgHours = d3.sum(d3.entries(hoursStats), function(d) {
+      return +d.key * d.value;
+    }) / d3.sum(d3.entries(hoursStats), function (d) { return d.value});
+
+    var avg = chart.append('g')
+        .datum(d3.round(avgHours, 1))
+        .attr('class', 'avg')
+        .attr('transform', function (d) {
+          return 'translate(' + x(d) + ',0)';
+        });
+    avg
+      .append('path')
+        .attr('stroke-dasharray', '5,5')
+        .attr('d', 'M0,-10L0,' + height);
+    avg
+      .append('text')
+        .attr('dx', 3)
+        .attr('dy', -5)
+        .text(function (d) { return 'Average: ' + d + ' hours'; });
+  }
 
   function makeCharts(stats) {
     // Subtract 10 from width because of right border on body
@@ -119,6 +212,10 @@ $(function() {
 
   $(window).on('resize', function() {
     $statsContainer.empty();
+
+    if (allStats.hasOwnProperty('hours'))
+      hoursChart(allStats['hours']);
+
     makeCharts(sortedStats);
   });
 
