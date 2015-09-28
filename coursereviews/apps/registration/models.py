e73bfdae83e91b0ba4c9f5754bf3b94b0de8561c
Dana Silver <dsilver1221@gmail.com>
2015-09-28 12:57:16 -0400

    Fix how registration.models gets the User model.
    `get_user_model` can't be called before the django app is setup (import
    time) so it is called in the methods or referenced with
    `settings.AUTH_USER_MODEL` for foreign keys.

--- coursereviews/apps/registration/models.py
+++ coursereviews/apps/registration/models.py
@@ -4,12 +4,7 @@ import random
 import re
 
 from django.conf import settings
-try:
-    from django.contrib.auth import get_user_model
-except ImportError: # django < 1.5
-    from django.contrib.auth.models import User
-else:
-    User = settings.AUTH_USER_MODEL
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.db import transaction
 from django.template.loader import render_to_string
@@ -81,6 +76,7 @@ class RegistrationManager(models.Manager):
         user. To disable this, pass ``send_email=False``.
 
         """
+        User = get_user_model()
         new_user = User.objects.create_user(username, email, password)
         new_user.is_active = False
         new_user.save()
@@ -151,6 +147,8 @@ class RegistrationManager(models.Manager):
         be deleted.
 
         """
+        User = get_user_model()
+
         for profile in self.all():
             try:
                 if profile.activation_key_expired():
@@ -179,7 +177,7 @@ class RegistrationProfile(models.Model):
     """
     ACTIVATED = u"ALREADY_ACTIVATED"
 
-    user = models.OneToOneField(User, verbose_name=_('user'))
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_('user'))
     activation_key = models.CharField(_('activation key'), max_length=40)
 
     objects = RegistrationManager()
