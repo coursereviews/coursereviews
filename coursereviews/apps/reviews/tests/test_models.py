303a613794f53948d815db90f45f62491421bfdf
Dana Silver <dsilver1221@gmail.com>
2015-09-30 20:44:05 -0400

    Add tests for reviews.models.Professor.

--- coursereviews/apps/reviews/tests/test_models.py
+++ coursereviews/apps/reviews/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from reviews.models import Department
+from reviews.models import (Department,
+                            Professor)
 
 class DepartmentTestCase(TestCase):
     def setUp(self):
@@ -24,3 +25,52 @@ class DepartmentTestCase(TestCase):
     def test_absolute_url(self):
         film = Department.objects.get(name='Film & Media Culture')
         self.assertEqual(film.get_absolute_url(), '/#film-media-culture')
+
+class ProfessorTestCase(TestCase):
+    def setUp(self):
+        dept = Department.objects.create(name='Computer Science')
+        Professor.objects.create(
+            first='FirstName',
+            last='LastName',
+            dept=dept,
+            email='prof@middlebury.edu',
+        )
+
+    def test_first_name(self):
+        prof = Professor.objects.get(last='LastName')
+        self.assertEqual(prof.first, 'FirstName')
+
+    def test_last_name(self):
+        prof = Professor.objects.get(last='LastName')
+        self.assertEqual(prof.last, 'LastName')
+
+    def test_department(self):
+        prof = Professor.objects.get(last='LastName')
+        dept = Department.objects.get(name='Computer Science')
+        self.assertEqual(prof.dept, dept)
+        self.assertEqual(dept.professors.all()[0], prof)
+
+    def test_email(self):
+        prof = Professor.objects.get(last='LastName')
+        self.assertEqual(prof.email, 'prof@middlebury.edu')
+
+    def test_slug(self):
+        prof = Professor.objects.get(last='LastName')
+        self.assertEqual(prof.slug, 'firstname-lastname')
+
+    def test_lookup(self):
+        prof = Professor.objects.get(last='LastName')
+        self.assertEqual(prof.lookup, 'FirstName LastName')
+
+    def test_natural_key(self):
+        prof = Professor.objects.get_by_natural_key('LastName')
+        self.assertEqual(prof.first, 'FirstName')
+
+    def test_absolute_url(self):
+        prof = Professor.objects.get(last='LastName')
+        self.assertEqual(prof.get_absolute_url(),
+                         '/professor/firstname-lastname')
+
+    def test_unicode(self):
+        prof = Professor.objects.get(last='LastName')
+        self.assertEqual(unicode(prof), 'FirstName LastName')
