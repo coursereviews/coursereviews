39b0f5817f42c013f16583d4c4569d1644ca0f9a
Dana Silver <dsilver1221@gmail.com>
2015-11-05 02:16:58 -0500

    Add getCookie to detail.js since we now wrap modules in IIFE.

--- coursereviews/static/reviews/js/detail.js
+++ coursereviews/static/reviews/js/detail.js
@@ -1,3 +1,19 @@
+// Get cookie for csrf token
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 $(function() {
 
   var questionLookup = d3.map({
