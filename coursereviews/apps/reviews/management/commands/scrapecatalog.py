5b8f7faa8d773ac761e65ffff97d99cff2a736f1
Dana Silver <dsilver1221@gmail.com>
2015-12-29 16:23:28 -0500

    Finish scrapecatalog command and add tests.

--- coursereviews/apps/reviews/management/commands/scrapecatalog.py
+++ coursereviews/apps/reviews/management/commands/scrapecatalog.py
@@ -1,16 +1,27 @@
 from __future__ import print_function
 
 from django.core.management.base import BaseCommand, CommandError
-from optparse import make_option
+from django.utils.termcolors import colorize
+from lxml import html
+from lxml.cssselect import CSSSelector
 from catalog.scraper import Scraper
 from catalog.models import Term
+import re
+import requests
+from reviews.models import Professor, Course, ProfCourse, Department
 
 class Command(BaseCommand):
     help = 'Adds courses from the specified term to the database.'
 
-    option_list = BaseCommand.option_list + (
-        make_option('--term', help='Specify the term. Ex: 201590'),
-    )
+    def __init__(self, *args, **kwargs):
+        self.professors_added = 0
+        self.courses_added = 0
+        self.prof_courses_added = 0
+
+        super(Command, self).__init__(*args, **kwargs)
+
+    def add_arguments(self, parser):
+        parser.add_argument('--term', help='Specify the term. Ex: 201590')
 
     @staticmethod
     def validate_term(term):
@@ -37,8 +48,117 @@ class Command(BaseCommand):
         scraper.xml_from_url()
         return scraper.create_catalog()
 
-    def handle(self, *args, **kwargs):
-        term = self.validate_term(kwargs['term'])
+    @staticmethod
+    def get_professor_by_id(id):
+        url = 'https://directory.middlebury.edu/GetRecord.aspx'
+        res = requests.get(url, params={'webid': id})
+
+        tree = html.fromstring(res.text)
+
+        if not CSSSelector('tr')(tree)[0].text_content().strip():
+            return None
+
+        fields = []
+        for tr in CSSSelector('tr')(tree):
+            tds = tr.getchildren()
+            fields.append((tds[0].text_content().strip(),
+                           tds[1].text_content().strip()))
+
+        return dict(fields)
+
+    @staticmethod
+    def course_is_skippable(course):
+        conditions = (
+            lambda: re.search('please register via', course.title, re.IGNORECASE),
+            lambda: re.search('please register for', course.title, re.IGNORECASE),
+            lambda: not any(course.instructors),
+        )
+
+        return any([condition() for condition in conditions])
+
+    def get_or_create_professors(self, course):
+        professors = []
+
+        for instructor in course.instructors:
+            try:
+                professor = Professor.objects.get(midd_webid=instructor.id)
+            except Professor.DoesNotExist:
+                professor_details = self.get_professor_by_id(instructor.id)
+
+                if professor_details and 'E-mail' in professor_details:
+                    email = professor_details['E-mail']
+                else:
+                    print(colorize('Unable to get email for Professor {}.'
+                                   .format(instructor.name), fg='yellow'))
+                    email = None
+
+                if professor_details and 'Department' in professor_details:
+                    department = professor_details['Department']
+                else:
+                    department = course.department.text
+
+                professor = Professor.objects.create(
+                    midd_webid=instructor.id,
+                    first=' '.join(instructor.name.split(' ')[:-1]),
+                    last=instructor.name.split(' ')[-1],
+                    email=email,
+                    dept=Department.objects.get_or_create(name=department)[0]
+                )
+
+                self.professors_added += 1
+                print(colorize('Added Professor {}.'.format(instructor.name), fg='blue'))
+
+            professors.append(professor)
+
+        return professors
+
+    def get_or_create_course(self, course):
+        code_regex = re.compile(r'([A-Z]+[\d]+).*')
+        code = code_regex.match(course.code).groups()[0]
+
+        try:
+            course = Course.objects.get(code=code)
+        except Course.DoesNotExist:
+            if course.description:
+                description = html.fromstring(course.description).text_content()
+            else:
+                description = None
+
+            course = Course.objects.create(
+                code=code,
+                title=course.title,
+                description=description,
+                dept=Department.objects.get_or_create(name=course.department.text)[0]
+            )
+
+            self.courses_added += 1
+            print(colorize('Added Course {}.'.format(code), fg='blue'))
+        return course
+
+    def handle(self, *arguments, **options):
+        term = self.validate_term(options['term'])
         catalog = self.create_catalog(term.id)
 
-        print(catalog)
+        for course in catalog:
+            if self.course_is_skippable(course):
+                continue
+
+            professors = self.get_or_create_professors(course)
+            course = self.get_or_create_course(course)
+
+            for professor in professors:
+                profcourse = ProfCourse.objects.get_or_create(
+                    prof=professor,
+                    course=course
+                )
+
+                if profcourse[1]:
+                    self.prof_courses_added += 1
+                    print(colorize('Added ProfCourse ({} {}, {}).'
+                                   .format(professor.first, professor.last, course.code),
+                                   fg='blue'))
+
+        print(colorize('Finished scraping term {}.'.format(options['term']), fg='green'))
+        print(colorize('  Added {} Courses.'.format(self.courses_added), fg='blue'))
+        print(colorize('  Added {} Professors.'.format(self.professors_added), fg='blue'))
+        print(colorize('  Added {} ProfCourses'.format(self.prof_courses_added), fg='blue'))
--- coursereviews/apps/reviews/tests/test_scrapecatalog_command.py
+++ coursereviews/apps/reviews/tests/test_scrapecatalog_command.py
@@ -1,6 +1,8 @@
+from catalog.models import Course, Instructor
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.test import TestCase
+from reviews.management.commands.scrapecatalog import Command as ScrapeCatalog
 
 class ScrapecoursesTest(TestCase):
     def test_no_term(self):
@@ -19,3 +21,57 @@ class ScrapecoursesTest(TestCase):
     def test_term_bad_season(self):
         self.assertRaises(CommandError,
                           lambda: call_command('scrapecatalog', term='201501'))
+
+    def test_create_catalog(self):
+        catalog = ScrapeCatalog.create_catalog('201590')
+        self.assertEqual(len(catalog.courses), 894)
+
+    def test_get_professor_by_id(self):
+        test_id = '1324ccafb6640fbdabc0c1f337a537d1'
+        fields = ScrapeCatalog.get_professor_by_id(test_id)
+
+        self.assertEqual(fields['Department'], 'Chemistry and Biochemistry')
+        self.assertEqual(fields['E-mail'], 'kjewett@middlebury.edu')
+
+    def test_get_professor_by_id_wrong_id(self):
+        test_id = '1'
+        fields = ScrapeCatalog.get_professor_by_id(test_id)
+
+        self.assertEqual(fields, None)
+
+    def test_course_is_skippable_no_professors(self):
+        course = Course()
+        course.title = 'A valid title'
+        course.instructors = [None]
+        skippable = ScrapeCatalog.course_is_skippable(course)
+
+        self.assertTrue(skippable)
+
+    def test_course_is_skippable_please_register(self):
+        course = Course()
+        course.title = 'Please register via AMST0101'
+        course.instructors = [Instructor()]
+        self.assertTrue(ScrapeCatalog.course_is_skippable(course))
+
+        course_strange_case = Course()
+        course_strange_case.title = 'Please RegisteR via AMST0101'
+        course_strange_case.instructors = [Instructor()]
+        self.assertTrue(ScrapeCatalog.course_is_skippable(course_strange_case))
+
+        course_for = Course()
+        course_for.title = 'Please register for AMST0101'
+        course_for.instructors = [Instructor()]
+        self.assertTrue(ScrapeCatalog.course_is_skippable(course_for))
+
+        course_for_strange_case = Course()
+        course_for_strange_case.title = 'Please RegisteR for AMST0101'
+        course_for_strange_case.instructors = [Instructor()]
+        self.assertTrue(ScrapeCatalog.course_is_skippable(course_for_strange_case))
+
+    def test_course_is_skippable_ok_course(self):
+        course = Course()
+        course.title = 'A valid title'
+        course.instructors = [Instructor()]
+        skippable = ScrapeCatalog.course_is_skippable(course)
+
+        self.assertFalse(skippable)
